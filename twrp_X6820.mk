1b23741df1cf745fc0dc00dbee99f466d9cbca76
hunter2986767 <156557124+hunter2986767@users.noreply.github.com>
2025-04-03 08:32:42 +0800

    Update twrp_X6820.mk

--- twrp_X6820.mk
+++ twrp_X6820.mk
@@ -8,14 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from X6820 device
 $(call inherit-product, device/infinix/X6820/device.mk)
 
 PRODUCT_DEVICE := X6820
-PRODUCT_NAME := lineage_X6820
+PRODUCT_NAME := twrp_X6820
 PRODUCT_BRAND := Infinix
 PRODUCT_MANUFACTURER := infinix
 PRODUCT_MODEL := Infinix Zero Ultra
